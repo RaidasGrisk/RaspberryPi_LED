975f5f9a78c489817ccf6aafdd063a12cc420430
RaidasGrisk <mrraidas@gmail.com>
2020-12-03 16:40:16 +0200

    dir structure + backend

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Color msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import Color from './components/color.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Color
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
